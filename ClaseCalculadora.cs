fa00a0e696df92754c4c08244091cd449808492c
merienditas <maricersal@hotmail.com>
2022-02-14 12:54:32 +0100

    Esqueleto clase Calculadora hecho

---
+++ ClaseCalculadora.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace calculadora
+{
+    class calculadora
+    {
+        int num1=0;
+        int num2=0;
+        int resultado=0;
+        
+      //escribir por pantalla los resultados
+    
+
+    public void Sumar(int num1, int num2)
+    {
+        int resultado = num1 + num2;
+    }
+
+    public void Restar(int num1, int num2)
+    {
+        int resultado = num1- num2;
+    }
+
+    public void Multiplicar(int num1, int num2)
+    {
+        int resultado = num1 * num2;
+    }
+   
+    public void Dividir(int num1, int num2)
+    {
+        int resultado = num1/num2; 
+    }
+
+    } 
+}
+
+
