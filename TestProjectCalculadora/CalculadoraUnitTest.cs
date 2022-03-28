5a214934415114ca725b374b21ffe04aef65cd6e
Tai55 <jesus.393hernandez@gmail.com>
2022-03-28 13:22:56 +0200

    test resta

--- TestProjectCalculadora/CalculadoraUnitTest.cs
+++ TestProjectCalculadora/CalculadoraUnitTest.cs
@@ -1,14 +1,21 @@
 using System;
 using Xunit;
+using calculadora;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 
 namespace TestProjectCalculadora
 {
     public class CalculadoraUnitTest
     {
         [Fact]
-        public void Test1()
+        public int Test1(int numero1, int numero2)
         {
-
+            int resultado = numero1- numero2;
+            return resultado;
         }
+    
+
+    
     }
 }
